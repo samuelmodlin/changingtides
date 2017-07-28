467a5dbdc7619cd3f60911cdf87fc969756f212a
Sam <samuelmodlin@users.noreply.github.com>
2017-07-28 01:20:24 -0700

    Changes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var express = require('express');
-  app = express(),
-  path = require("path"),
-  bodyParser = require('body-parser'),
-  send = require('gmail-send')({
-    user: 'changingtideshaiti@gmail.com',
-    pass: 'orphanage11',
-    to:   'benjamin_modlin@branson.org',
-    subject: 'test subject',
-    text:    'HTML Not supported... contact samuel_modlin@branson.org',         // Plain text
-    html:    '<b>html text</b>'
-  });
+var app = express();
+var path = require("path");
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'OAuth2',
+        user: 'changingtideshaiti@gmail.com',
+        clientId: '504109041359-m27ngu5prtsim73h9lfj1ju7rgq0j7u3.apps.googleusercontent.com',
+        clientSecret: 'suzKgknkbTYo7bsU4N5UrCp6',
+        refreshToken: '1/DNy5hlI6tEndi4ZLCS8rz0xdysYGQ9Hk0xw5hWi8EL4',
+        accessToken: 'ya29.GluVBLpcsSs5VY4nAqfTjO4XuQ0CLCbewdTUQlLglnX92XuxyXYtv6OHd9iRo23RAHZekGL5PKxQy7j73m65FoFA5OOto6jiRkHzlbJjJm03Xoz2B6vmxYFB-F5D',
+    }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -40,11 +47,18 @@ app.post('/contact/submit', function(req, res) {
   var message = req.body.message;
   var reason = req.body.reason;
 
-  send({
-    subject: 'Auto Generated Form ' + Math.floor((Math.random() * 100000000000000) + 1),
-    html: '<h1 style = "color: red">Changing Tides Orphanage Contact Form Submittion:</h1><br><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Name: ' + name + '</h2><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Email: ' + email + '</h2><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Reason: ' + reason + '</h2><div><h2>Message: ' + message + '</h2></div>',
-  }, function (err, res) {
-    console.log('* [example 1.1] send() callback returned: err:', err, '; res:', res);
+  let mailOptions = {
+      from: '"Changing Tides" <changingtideshaiti@gmail.com>',
+      to: 'modlin.sam@gmail.com',
+      subject: 'Auto Generated Form ' + Math.floor((Math.random() * 100000000000000) + 1),
+      html: '<h1 style = "color: red">Changing Tides Orphanage Contact Form Submittion:</h1><br><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Name: ' + name + '</h2><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Email: ' + email + '</h2><h2 style = "border-bottom: 2px dotted black; border-top: 2px dotted black">Reason: ' + reason + '</h2><div><h2>Message: ' + message + '</h2></div>',
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
   });
 
 });
